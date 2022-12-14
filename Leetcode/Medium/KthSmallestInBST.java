b3e5f72a44c0bb3ce75e2d6f3723e4a55f7234da
Tyree Chrisp <chri.tyree@gmail.com>
2022-12-14 15:38:50 -0800

    Added two leetcode questions #236 and #230
    Same as title.

---
+++ Leetcode/Medium/KthSmallestInBST.java
@@ -0,0 +1,79 @@
+/*
+    12/08/2022
+
+    230. Kth Smallest Element in a BST
+    https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+    BST
+        - ordered, not balanced
+        - smallest on leftmost side
+        - head is "midpoint"
+    #BST #InorderTraversal
+
+    Approach:
+        1) Find the smallest element #1
+        2) Do inorder traversal w/ counter once current node is null/at leaf node.
+
+        (2)                 (4)                         (5)
+      (1)  (3)           (2)    (5)                 (3)     (6)
+                       (1) (3)                    (2) (4)
+                                                (1)
+
+Timeline:
+    Implemented and passed 3 basic testcases in 12:00 min.
+    Submitted at 15:45 min.
+
+Difficulty: Medium
+
+Big O Analysis
+Time: Beat 100%. O(n)
+Space: Beat 86.62% O(1)
+
+Discussion:
+    Q: If insert and delete on tree is often. how do we optimize to find the kth smallest element frequently?
+    A: With each insert or delete on the left branch of current kth smalelst, we update a variable the tracks
+    the kth smallest node.
+
+    Further topic: Threaded binary tree is useful for faster accessing ancestors in tree without stacks or recursion.
+                    Helpful in inorder traversal.
+                    No null nodes. Nodes with no children point to the inorder predecessor, if left leaf, or
+                    inorder successor, if right leaf.
+*/
+
+class KthSmallestInBST {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+        int c = 0;
+        public int kthSmallest(TreeNode root, int k) {
+            return kthSmallestSearch(root, k);
+        }
+
+        public int kthSmallestSearch(TreeNode root, int k) {
+            if(root.left != null) {
+                int val = kthSmallestSearch(root.left, k);
+                if(val != -1) return val;
+            }
+            c++;
+            if(c == k) {
+                return root.val;
+            }
+            if(root.right != null) {
+                int val = kthSmallestSearch(root.right, k);
+                if(val != -1) return val;
+            }
+            return -1;
+        }
+    }
---
+++ Leetcode/Medium/LCAofBinaryTree.java
@@ -0,0 +1,106 @@
+/*
+
+    12/09/2022
+
+    Least Common Ancestor of Binary Tree
+    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+
+    BST
+        - Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+        – Unordered
+
+    Approach:
+        1) Find one target node, add to queue* in postorder traversal DFS.
+        2) Find the second target node, add to a second queue in postorder traversal DFS.
+        3) The queue size represents the level of the node. Poll queues until the sizes are equal.
+        4) Compare the nodes at the head of each queue until the match, then return matching node.
+
+    Timeline:
+        13:00            Implemented solution. Failed 1 of 3 basic test cases.
+        17:30            Recognized misuse of data structure and switched from Stack to Queue.
+        TIMED OUT(20:00) Debugging
+        +5min            Fixed misspelled variable that was used in comparison logic. NOTE: Make variable names more
+                            distinct. pQue and qQue are easy to overlook during debugging.
+
+
+    Big O Analysis
+        Time: Beats 55.11% O(2N) ~> O(N)
+        Space: Beats 12.29% O(2N) ~> O(N)
+
+    Discussion:
+        Space optimization:
+            Optimize by only using one queue. Build queue on one target node like what is done here.
+        When second node is found, instead of building onto another queue, begin to poll from first queue, and do the comparison
+        during the "return to root" phase of the recursion.
+
+        Time optimization:
+            Instead of starting from root node again for second node, do a single full DFS looking for both nodes.
+*/
+
+public class LCAofBinaryTree {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ // double dfs w/ postorder traversal
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Queue<TreeNode> pQue = new LinkedList<>();
+        Queue<TreeNode> qQue = new LinkedList<>();
+        dfs(root, p, pQue);
+        Queue<TreeNode> newPQue = new LinkedList<>();
+        // while(pQue.size() > 0) {
+        //     TreeNode node = pQue.poll();
+        //     System.out.print(node.val + " -> ");
+        //     newPQue.offer(node);
+        // }
+        // System.out.println(" ");
+         dfs(root, q, qQue);
+         Queue<TreeNode> newQQue = new LinkedList<>();
+        // while(qQue.size() > 0) {
+        //     TreeNode node = qQue.poll();
+        //     System.out.print(node.val + " -> ");
+        //     newQQue.offer(node);
+        // }
+        // System.out.println(" ");
+        while(qQue.size() > pQue.size()) {
+            qQue.poll();
+        }
+        while(pQue.size() > qQue.size()) {
+            pQue.poll();
+        }
+        while(pQue.size() > 0 && qQue.size() > 0 && pQue.peek() != qQue.peek()) {
+            qQue.poll();
+            pQue.poll();
+        }
+        if(pQue.size() == 0 || qQue.size() == 0) return root;
+        return pQue.poll();
+    }
+
+    public static void dfs(TreeNode root, TreeNode target, Queue<TreeNode> que) {
+        if(root.val == target.val) {
+            que.offer(root);
+            return;
+        }
+        if(root.left != null) {
+            dfs(root.left, target, que);
+            if(que.size() != 0) {
+                que.offer(root);
+                return;
+            }
+        }
+        if(root.right != null) {
+            dfs(root.right, target, que);
+            if(que.size() != 0) {
+                que.offer(root);
+                return;
+            }
+        }
+        return;
+    }
+}
