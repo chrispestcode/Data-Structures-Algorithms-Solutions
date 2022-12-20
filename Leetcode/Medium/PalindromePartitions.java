28689200e64657590621a2c07237e37b25db7ffe
Tyree Chrisp <chri.tyree@gmail.com>
2022-12-19 23:57:05 -0800

    Create PalindromePartitions.java

---
+++ Leetcode/Medium/PalindromePartitions.java
@@ -0,0 +1,32 @@
+
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> pals = new ArrayList<>();
+        int l = s.length() / 2;
+        int r = l;
+        if(s.length() % 2 == 0 ) {
+            l -= 1;
+        }
+        while(l >= 0 && r < s.length()) {
+            checkPartition(s, l , r, false, pals);
+            l--;
+            r++;
+        }
+        return pals;
+    }
+
+    public void checkPartition(String s, int l, int r, boolean isPalindrome, List<List<String>> pals) {
+        List<String> pal = new ArrayList<>();
+        if(l > r || l < 0 || r >= s.length()) return;
+        if(s.charAt(l) == s.charAt(r)) {
+            //checkPartition
+            pal.add(s.substring(l,r+1));
+            checkPartition(s, l-1, r, true, pals);
+            checkPartition(s, l, r+1, true, pals);
+            checkPartition(s, l-1, r+1, true,pals);
+        }
+        pals.add(pal);
+        return;
+    }
+}
